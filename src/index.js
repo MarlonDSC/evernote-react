6f57b72a51c06ab92b06095be3323a2586091491
MarlonDSC <marlonsubuyu.2012@gmail.com>
2022-03-05 10:25:22 -0400

    first commit for react-evernote

--- src/index.js
+++ src/index.js
@@ -3,12 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// const firebase = require('firebase');
+// require('firebase/firestore');
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA5SHL_qEP4WYGZtoij0McDw91Ghu-y3V8",
+//   authDomain: "react-app-1a999.firebaseapp.com",
+//   projectId: "react-app-1a999",
+//   storageBucket: "react-app-1a999.appspot.com",
+//   messagingSenderId: "557948253830",
+//   appId: "1:557948253830:web:889b73d1a35f03cf2a0307",
+//   measurementId: "G-MPTRML3NSN"
+// };
+//firebase.initializeApp(firebaseConfig);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA5SHL_qEP4WYGZtoij0McDw91Ghu-y3V8",
+  authDomain: "react-app-1a999.firebaseapp.com",
+  projectId: "react-app-1a999",
+  storageBucket: "react-app-1a999.appspot.com",
+  messagingSenderId: "557948253830",
+  appId: "1:557948253830:web:889b73d1a35f03cf2a0307",
+  measurementId: "G-MPTRML3NSN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('evernote-cointaner')
 );
 
 // If you want to start measuring performance in your app, pass a function
